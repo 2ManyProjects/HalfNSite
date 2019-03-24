aed7d05118552f53fa2184a12d4854193516cef8
2ManyProjects <shaivkamat@gmail.com>
2019-03-23 20:18:52 -0700

    Updates

---
+++ precache-manifest.999514cfdc8c98fe609fa5f627a80443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f63eab9456d970d486e302eca06e0acc",
+    "url": "/HalfNSite/static/media/bestbuy.f63eab94.png"
+  },
+  {
+    "revision": "b209618a63680672089c",
+    "url": "/HalfNSite/static/js/runtime~main.2b0ce411.js"
+  },
+  {
+    "revision": "5ed04a88c7f641502462",
+    "url": "/HalfNSite/static/js/main.b4d135c3.chunk.js"
+  },
+  {
+    "revision": "a8c72c82905698fb1c14",
+    "url": "/HalfNSite/static/js/2.84ca3213.chunk.js"
+  },
+  {
+    "revision": "5ed04a88c7f641502462",
+    "url": "/HalfNSite/static/css/main.3d9f198a.chunk.css"
+  },
+  {
+    "revision": "a8c72c82905698fb1c14",
+    "url": "/HalfNSite/static/css/2.3fe24777.chunk.css"
+  },
+  {
+    "revision": "e1987fee56ae1097689748936a92c03a",
+    "url": "/HalfNSite/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HalfNHalf/precache-manifest.c9da3dff6207ce470dc7df8cb014f2e5.js"
+  "/HalfNSite/precache-manifest.999514cfdc8c98fe609fa5f627a80443.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/HalfNHalf/index.html", {
+workbox.routing.registerNavigationRoute("/HalfNSite/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
